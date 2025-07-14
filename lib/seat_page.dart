9f3ebcc308d50f8948473c50439494bb6bfcd749
leeys <95343772+agent-megurii@users.noreply.github.com>
2025-07-14 16:08:26 +0900

    feat: SeatPage 좌석 20행 레이아웃 및 행번호 가운데 배치

--- lib/seat_page.dart
+++ lib/seat_page.dart
@@ -4,7 +4,7 @@ class SeatPage extends StatelessWidget {
   final String departureStation;
   final String arrivalStation;
 
-  SeatPage({
+  const SeatPage({
     super.key,
     required this.departureStation,
     required this.arrivalStation,
@@ -12,10 +12,80 @@ class SeatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text('좌석 선택')),
-      body: Center(child: Text('$departureStation -> $arrivalStation')),
+      appBar: AppBar(title: const Text('좌석 선택')),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        children: [
+          // ABCD 레이블
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              'A',
+              'B',
+              '',
+              'C',
+              'D',
+            ].map((e) => seatLabel(e)).toList(),
+          ),
+
+          const SizedBox(height: 20),
+
+          // 좌석 행 20개 생성
+          // 여기에 List.generate(20, (index) { ... })
+          ...List.generate(20, (index) {
+            return Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  seatBox(),
+                  SizedBox(width: 4),
+                  seatBox(),
+                  SizedBox(width: 4),
+                  //행번호 컨테이너
+                  Container(
+                    width: 50,
+                    height: 50,
+                    alignment: Alignment.center,
+                    child: Text('${index + 1}', style: TextStyle(fontSize: 18)),
+                  ),
+
+                  seatBox(),
+                  SizedBox(width: 4),
+
+                  seatBox(),
+
+                  // ...['A', 'B', 'C', 'D'].map((seat) => seatBox()).toList(),
+                ],
+              ),
+            );
+          }),
+        ],
+      ),
     );
   }
+
+  // ABCD 레이블을 표시하는 위젯
+  Widget seatLabel(String label) {
+    return Container(
+      width: 50,
+      height: 50,
+      alignment: Alignment.center,
+      margin: EdgeInsets.symmetric(horizontal: 4),
+      child: Text(label, style: TextStyle(fontSize: 18)),
+    );
+  }
+}
+
+Widget seatBox() {
+  return Container(
+    width: 50,
+    height: 50,
+    margin: EdgeInsets.symmetric(horizontal: 4), // 좌석 사이 가로 간격
+    decoration: BoxDecoration(
+      color: Colors.grey[300],
+      borderRadius: BorderRadius.circular(8),
+    ),
+  );
 }
